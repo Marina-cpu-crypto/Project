78c7eb3cd0dde9c06d2a3a3570085d3677812a27
marins <marina.khodova.06@mail.ru>
2025-06-10 12:50:44 +0300

    Методы форм для отображения данных из классов на экран

--- FormTour.cs
+++ FormTour.cs
@@ -28,13 +28,33 @@ namespace Project
 
         private void FormTour_Load(object sender, EventArgs e)
         {
+            ShowListFreeTours();
+            //ShowListFreeTours();
+            //foreach (var t in freetours)
+            //{
+            //    
+            //    //listBoxFreeTours.Items.Add(t);
+            //}
+
+        }
+        void ShowListFreeTours()
+        {
+            listBoxGetTours.Items.Clear();
             foreach (var t in freetours)
             {
                 listBoxFreeTours.Items.Add(t);
             }
             
         }
+        void ShowListGetTours()
+        {
+            listBoxGetTours.Items.Clear();
+            foreach (var t in gettours)
+            {
+                listBoxGetTours.Items.Add(t.toursname + " " + t.name + " " + t.day + " " + t.month + " " + t.year);
 
+            }
+        }
         private void listBoxFreeTours_SelectedIndexChanged(object sender, EventArgs e)
         {
             int currentIndex = listBoxFreeTours.SelectedIndex;
@@ -53,22 +73,25 @@ namespace Project
             freetours.RemoveAt(currentIndex);
             listBoxFreeTours.Items.Clear();
             listBoxGetTours.Items.Clear();
-            foreach (var t in freetours)
-            {
-                listBoxFreeTours.Items.Add(t);
-            }
-            foreach (var t in gettours)
-            {
-                listBoxGetTours.Items.Add(t.toursname + " " + t.name + " " + t.day + " " + t.month + " " + t.year);
 
-            }
+            ShowListFreeTours();
+            ShowListGetTours();
+            //foreach (var t in freetours)
+            //{
+            //    listBoxFreeTours.Items.Add(t);
+            //}
+            //foreach (var t in gettours)
+            //{
+            //    listBoxGetTours.Items.Add(t.toursname + " " + t.name + " " + t.day + " " + t.month + " " + t.year);
+
+            //}
         }
         private void buttonCheck_Click(object sender, EventArgs e)
         {
             listBoxGetTours.Items.Clear();
             foreach (var t in gettours)
             {
-                if (t.year >= todayyear && t.month >= todaymonth && t.day >= todayday) listBoxGetTours.Items.Add(t.toursname + " " + t.name + " " + t.day + " " + t.month + " " + t.year);
+                if (t.year >= todayyear && t.month >= todaymonth && t.day >= todayday) ShowListGetTours(); /*listBoxGetTours.Items.Add(t.toursname + " " + t.name + " " + t.day + " " + t.month + " " + t.year);*/
                 if (t.year == todayyear && t.month == todaymonth && (t.day == todayday || (t.day - todayday == 2))) MessageBox.Show("Скоро поездка: "+t.toursname);
             }
 
